657cdbd821f1c6cf390e935a96cc4fb7513f9933
Jules Laffitte <jules_laffitte@apple.com>
2023-01-21 12:53:39 -0800

    Adds PEP517 style builds
    * adds `pyproject.toml` file (see PEP518)
    * moves to a `src-layout` see:
      - https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#src-layout
      - https://packaging.python.org/en/latest/discussions/src-layout-vs-flat-layout/
    * updates `README.md` to match the actual version tested via workflow

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-with open("requirements.txt", "r") as f:
-    dependencies = list(map(lambda line: line.strip(), f.readlines()))
-
-with open("requirements-yaml.txt", "r") as f:
-    yaml_dependencies = list(map(lambda line: line.strip(), f.readlines()))
-
-setuptools.setup(
-    name="nectarine",
-    version="1.0.5",
-    author="Clément Doumergue",
-    author_email="clement.doumergue@etna.io",
-    description="Library to load configuration from various sources",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/doom/nectarine",
-    packages=setuptools.find_namespace_packages(include=["nectarine", "nectarine.*"]),
-    install_requires=dependencies,
-    extras_require={
-        "yaml": yaml_dependencies,
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+if __name__ == '__main__':
+    setup()
